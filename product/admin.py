26753f50fd3da9b71fc6b62b75a2ab3c14ed8baa
BakryDiarra <bakrydiarra@gmail.com>
2023-04-18 10:06:45 +0000

    Fix: mispelling setting.py add Class admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Allows admin to manage categories via the admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Allows admin to manage products via the admin panel"""
+    list_display = (
+        'sku',
+        'name',
+        'excerpt',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
