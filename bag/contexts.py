0b3dfc48ffff489000cf9e67aa1c7d0907f9243b
BakryDiarra <bakrydiarra@gmail.com>
2023-04-20 10:27:40 +0000

    Add: bag context

---
+++ bag/contexts.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from product.models import Product
+
+
+def bag_contents(request):
+    """ Bag context available to all templates """
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
+    }
+
+    return context
