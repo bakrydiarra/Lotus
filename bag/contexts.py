ffe033aefde1ff51bc8cfefcc133afc3931dc1ea
BakryDiarra <bakrydiarra@gmail.com>
2023-04-22 12:49:41 +0000

    Update: contextx.py delivery parameter and settings.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,6 +21,14 @@ def bag_contents(request):
             'product': product,
         })
 
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
     grand_total = delivery + total
 
     context = {
@@ -28,7 +36,10 @@ def bag_contents(request):
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+
     }
 
     return context
