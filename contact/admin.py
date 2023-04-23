fde459a785c48b9f4bb87bfdbb7648f8dbcad0eb
BakryDiarra <bakrydiarra@gmail.com>
2023-04-23 14:22:00 +0000

    Create: ContactUs view

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """Allows admin to manage enquiries via the admin panel"""
+    list_display = (
+        'name',
+        'email',
+        'message',
+        'reason',
+        'written_on',
+    )
+    list_filter = (
+        'name',
+        'reason',
+        'written_on'
+          )
+
+
+admin.site.register(Contact, ContactAdmin)
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib import messages
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+
+class ContactUs(CreateView):
+
+    """
+    class to show a contact form
+    """
+
+    model = Contact
+    form_class = ContactForm
+    template_name = 'contact.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(
+            self.request,
+            'Your message has been sent successfully!')
+        return response
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        messages.error(self.request, 'Please fill up the form completely.')
+        return response
