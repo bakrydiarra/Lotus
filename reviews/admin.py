d421b29c0a3ced522515d49995af7f70acb8fc77
BakryDiarra <bakrydiarra@gmail.com>
2023-04-28 12:39:51 +0000

    Add: class ReviewAdmin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Review
+from product.models import Category
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """Allows admin to manage Testimonials via the admin panel"""
+    list_display = (
+        'name',
+        'category',
+        'created_on',
+        'updated_on'
+    )
+
+
+admin.site.register(Review, ReviewAdmin)
