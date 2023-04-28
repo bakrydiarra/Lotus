c151cb744c9ff1528faa9718e90f4af5c489747f
BakryDiarra <bakrydiarra@gmail.com>
2023-04-28 10:21:52 +0000

    Add: confirmation email function ContactUs

--- contact/views.py
+++ contact/views.py
@@ -4,6 +4,9 @@ from django.views.generic import CreateView
 from django.contrib import messages
 from .models import Contact
 from .forms import ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
 
 
 class ContactUs(CreateView):
@@ -22,6 +25,31 @@ class ContactUs(CreateView):
         messages.success(
             self.request,
             'Your message has been sent successfully!')
+
+        # send confirmation email
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        subject = 'Thank you for contacting us'
+
+        # render subject and message templates
+        subject = render_to_string(
+            "contact/confirmation_emails/confirmation_email_subject.txt",
+            {"subject": subject},
+        )
+        message = form.cleaned_data['message']
+        message = render_to_string(
+            "contact/confirmation_emails/confirmation_email_body.txt",
+            {"name": name, "message": message},
+        )
+
+        # send email
+        send_mail(
+            subject,
+            message,
+            email,
+            [email],
+            fail_silently=False,
+        )
         return response
 
     def form_invalid(self, form):
