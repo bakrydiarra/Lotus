72026d8ddeca94b2ea6fc2c609a8e25be4a2982a
BakryDiarra <bakrydiarra@gmail.com>
2023-05-01 15:28:28 +0000

    Add: view, url and templates AddFaq

--- faq/urls.py
+++ faq/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', views.Faqs.as_view(), name='faqs'),
+    path('add/', views.AddFaq.as_view(), name='add_faq'),
 ]
--- faq/views.py
+++ faq/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Faq
 from django.views import generic, View
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import FaqForm
 
 
 class Faqs(generic.ListView):
@@ -9,3 +12,23 @@ class Faqs(generic.ListView):
     """
     model = Faq
     template_name = 'faqs/faqs.html'
+
+
+class AddFaq(CreateView):
+    """
+    Class to add a Faq
+    """
+    model = Faq
+    form_class = FaqForm
+    template_name = 'faqs/add_faq.html'
+    success_url = reverse_lazy('faqs')
+
+    def form_valid(self, form):
+        form.instance.name = self.request.user
+        self.request.user == self.request.user.is_superuser
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        messages.error(self.request, 'Please fill up all fields.')
+        return response
