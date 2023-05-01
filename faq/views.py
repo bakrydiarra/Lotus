7e93c6959763294d52d665c4d0e06e79fb07c332
BakryDiarra <bakrydiarra@gmail.com>
2023-05-01 11:57:45 +0000

    Add: code view Faqs

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Faq
+from django.views import generic, View
 
-# Create your views here.
+
+class Faqs(generic.ListView):
+    """
+    Class to show all the Faqs
+    """
+    model = Faq
+    template_name = 'faq/faqs.html'
