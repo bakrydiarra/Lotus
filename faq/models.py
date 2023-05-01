36c472208ba2f908635935b5bbd3fb142c73f941
BakryDiarra <bakrydiarra@gmail.com>
2023-05-01 15:00:23 +0000

    Add: name field to model Faq

--- faq/models.py
+++ faq/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Faq(models.Model):
     """Model for FAQ"""
 
+    name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="faqs")
     question = models.CharField(max_length=250, blank=False)
     answer = models.TextField()
 
