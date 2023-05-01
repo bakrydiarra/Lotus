d3857423d82f049365c4bd45fe673bd0329de6f7
BakryDiarra <bakrydiarra@gmail.com>
2023-05-01 11:33:45 +0000

    Add: class model Faq

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Faq(models.Model):
+    """Model for FAQ"""
+
+    question = models.CharField()
+    answer = models.TextField()
+
+    def get_absolute_url(self):
+        """Get url after owner adds/edits FAQ"""
+        return reverse('faqs')
+
