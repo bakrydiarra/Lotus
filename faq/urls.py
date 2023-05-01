82a61e3e465244f29ed987ef97e97797af586c59
BakryDiarra <bakrydiarra@gmail.com>
2023-05-01 16:24:48 +0000

    Add: url pattern

--- faq/urls.py
+++ faq/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('', views.Faqs.as_view(), name='faqs'),
     path('add/', views.AddFaq.as_view(), name='add_faq'),
+    path(
+        'edit_faq/<int:pk>/', views.EditFaq.as_view(), name='edit_faq'
+        ),
 ]
