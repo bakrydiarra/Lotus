ffb4f902f54189a9eda5389caf373ec0b8b1733f
BakryDiarra <bakrydiarra@gmail.com>
2023-05-02 13:48:16 +0000

    Add: about_us.html, view and url

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('about_us/', views.AboutUs.as_view(), name='about_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ from django.views import generic, View
 class Home(generic.TemplateView):
     """This view is used to display the home page"""
     template_name = "home/index.html"
+
+
+class AboutUs(generic.TemplateView):
+    """This view is used to display the about us page"""
+    template_name = "home/about_us.html"
